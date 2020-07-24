7bb77b8966affaf77192b6424abbd334814083bc
Javier Vega <jave@microsoft.com>
2020-07-24 00:57:03 +0000

    remove duplicate

--- tools/oesign/oesign.c
+++ tools/oesign/oesign.c
@@ -35,29 +35,6 @@ typedef struct _optional_uint16
     uint16_t value;
 } optional_uint16_t;
 
-typedef struct _optional_oe_uuid_t
-{
-    bool has_value;
-    oe_uuid_t value;
-} optional_oe_uuid_t;
-
-typedef struct _optional_bool
-{
-    bool has_value;
-    bool value;
-} optional_bool_t;
-
-typedef struct _optional_uint64
-{
-    bool has_value;
-    uint64_t value;
-} optional_uint64_t;
-
-typedef struct _optional_uint16
-{
-    bool has_value;
-    uint16_t value;
-} optional_uint16_t;
 
 // Options loaded from .conf file. Uninitialized fields contain the maximum
 // integer value for the corresponding type.
